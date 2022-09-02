903a03b1909326b6ce5ae82ca7569a79889f4726
htach <72769068+htach@users.noreply.github.com>
2022-09-02 18:13:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ import httpx
 
 cookie = pathlib.Path('cookie.txt').read_text()
 
-limits = httpx.Limits(max_connections=10)
+limits = httpx.Limits(max_connections=5)
 client = httpx.AsyncClient(
     cookies={'.ROBLOSECURITY': cookie},
     limits=limits
 )
 
 
-async def fetch_csrf():
+async def fetch_csrf() -> None:
     req = await client.post('https://auth.roblox.com/v1/xbox/disconnect')
     return req.headers['x-csrf-token']
 
 
-async def clear_page(page_number):
+async def clear_page(page_number: int) -> None:
     req = await client.get(f'https://privatemessages.roblox.com/v1/messages?pageNumber={page_number}&pageSize=20&messageTab=Inbox')
     res = req.json()
 
@@ -25,10 +25,11 @@ async def clear_page(page_number):
     await client.post(
         'https://privatemessages.roblox.com/v1/messages/archive',
         data={"messageIds": msg_ids},
-        headers={'x-csrf-token': await fetch_csrf()})
+        headers={'x-csrf-token': await fetch_csrf()}
+    )
 
 
-async def main():
+async def main() -> None:
     req = await client.get('https://privatemessages.roblox.com/v1/messages?pageNumber=1&pageSize=20&messageTab=Inbox')
     res = req.json()
 
